11a113bbd2cab97bfd26efc48bd2275c92a3cfdf
Chiran Gurung <grgchiran08@yahoo.com>
2025-05-25 14:09:00 -0400

    Add all predictions page

--- frontend-ps/app/predictions/page.tsx
+++ frontend-ps/app/predictions/page.tsx
@@ -1,9 +1,24 @@
 import { title } from "@/components/primitives";
+import { predictions } from "../mockdata";
+import PredictionCard from "@/components/prediction-card";
 
 export default function PredictionsPage() {
   return (
     <div>
-      <h1 className={title()}>Predictions</h1>
+      <h1 className={title()}>All Predictions</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 my-20">
+        {predictions.map((prediction) => (
+          <PredictionCard
+            key={prediction.id}
+            category={prediction.category}
+            content={prediction.content}
+            creator={prediction.creator}
+            expirationTime={prediction.expirationTime}
+            verificationStatus={prediction.verificationStatus}
+            id={prediction.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
